989b4d2814ffa82576301265e92d929706733524
Paschoal Colombini Neto <108839486+Pcolombini@users.noreply.github.com>
2023-06-29 16:42:21 -0300

    Add files via upload

---
+++ sistema-gerenciamento-financeiro/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Sistema De Gerenciamento de Dívidas</title>
+	<link rel="stylesheet" href="CSS/style.css">
+	<link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	<link href="https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200;0,600;1,200;1,600&display=swap" rel="stylesheet">
+</head>
+<body>
+	<main class="principal">
+		<section class="container">
+			<form action="index_submit" method="" accept-charset="utf-8" class="forms">
+				<legend>Login</legend>
+				<input type="password" name="senha" placeholder="Senha">
+				<input type="submit" name="Entrar" value="Entrar">
+			</form>
+		</section>
+	</main>
+</body>
+</html>
